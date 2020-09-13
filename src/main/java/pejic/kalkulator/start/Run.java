56baadbfcd22999cfe62a1f915da58088c7e1ca0
Zeljko Pejic <zeljkopejic@yahoo.com>
2020-09-13 14:47:54 +0200

    Zapocet projekt kalkulator

---
+++ src/main/java/pejic/kalkulator/start/Run.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pejic.kalkulator.start;
+
+import pejic.kalkulator.view.Kalkulator;
+
+/**
+ *
+ * @author Pejić
+ */
+public class Run {
+    
+    public static void main(String[] args) {
+        
+        new Kalkulator().setVisible(true);
+    }
+    
+    
+    
+}
